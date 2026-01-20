a230e38707ce0982e615a0f698bb9d9b3ec1cd93
isratbiswas <isratbiswas28@gmail.com>
2026-01-20 08:00:12 +0600

    Task modules completed

--- src/app/modules/task/task.service.js
+++ src/app/modules/task/task.service.js
@@ -1,32 +1,32 @@
-// import { Task } from "./task.model";
+import { Task } from "./task.model";
 
-// const createTask = async (req, res) => {
-//   const task = await Task.create({
-//     title: req.body.title,
-//     user: req.user.id,
-//   });
-//   return task;
-// };
+const createTask = async (req, res) => {
+  const task = await Task.create({
+    title: req.body.title,
+    user: req.user.id,
+  });
+  return task;
+};
 
-// const getTasks = async () => {
-//   const tasks = await Task.find();
-//   return tasks;
-// };
+const getTasks = async () => {
+  const tasks = await Task.find();
+  return tasks;
+};
 
-// const updateTask = async (id, updateData) => {
-//   const updatedTask = await Task.findByIdAndUpdate(id, updateData, {
-//     new: true,
-//   });
-//   return updatedTask;
-// };
+const updateTask = async (id, updateData) => {
+  const updatedTask = await Task.findByIdAndUpdate(id, updateData, {
+    new: true,
+  });
+  return updatedTask;
+};
 
-// const deleteTask = async (id) => {
-//   await Task.findByIdAndDelete(id);
-// };
+const deleteTask = async (id) => {
+  await Task.findByIdAndDelete(id);
+};
 
-// export const TaskService = {
-//   createTask,
-//   getTasks,
-//   updateTask,
-//   deleteTask,
-// };
+export const TaskService = {
+  createTask,
+  getTasks,
+  updateTask,
+  deleteTask,
+};
